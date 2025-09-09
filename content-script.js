8c2d7923fbcfa15b850c6ecae4ac3e56cc2aee08
534 <sapisidhash@gmail.com>
2025-09-08 21:02:50 -0700

    content-script: Fix issue with Firefox Array conversion
    Due to context isolation that Firefox enforces, we were unable to
    modify message.body as the message object came from the injected
    script in the webpage. The solution was to copy the properties of
    the object into a new object, which we could then modify as needed.

--- content-script.js
+++ content-script.js
@@ -3,13 +3,18 @@ async function handle_hf_message(cs_message) {
 	const structured_message = cs_message.detail;
 	const message_id = structured_message.message_id;
 	const message = structured_message.message;
+	const xmessage = {};
+	xmessage.body = message.body;
+	xmessage.url = message.url;
+	xmessage.headers = message.headers;
+	xmessage.method = message.method;
 	if (typeof message.body === "object") {
-		message.body = message.body.toString().split(","); // convert uint8array -> array
+		xmessage.body = xmessage.body.toString().split(","); // convert uint8array -> array
 	}
 	// We want to send the message upstream to the background script, and get the response
 	const srb_structured_message = {
 		action: "proxy",
-		content: message
+		content: xmessage
 	};
 	const response = await browser.runtime.sendMessage(srb_structured_message);
 	const finresponse = JSON.stringify(response);
